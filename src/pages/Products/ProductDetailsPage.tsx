fe2b1bd1a0eaeb73cdfdf6f2d8d169bf3572c8e9
Jelena <jelena-miodragovic@live.com>
2025-05-23 21:28:09 +0200

    add product details

--- src/pages/Products/ProductDetailsPage.tsx
+++ src/pages/Products/ProductDetailsPage.tsx
@@ -1,5 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { fetchProductById } from '@/api/productsAPI';
+import useFetch from '@/hooks/useFetch';
+import type { Product } from '@/types/product';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '@/slices/cartSlice';
+import { useCallback } from 'react';
+
 const ProductDetailsPage = () => {
-  return <div>Product details page</div>;
+  const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch();
+
+  const fetchProduct = useCallback(() => fetchProductById(id!), [id]);
+  const { data: product, loading, error } = useFetch<Product>(fetchProduct);
+
+  if (loading) return <p>Loading...</p>;
+  if (error || !product) return <p>Error fetching product details.</p>;
+
+  return (
+    <div className="product-details">
+      <img src={product.image} alt={product.title} width="200" />
+      <h2>{product.title}</h2>
+      <p>
+        <strong>Price:</strong> {product.price} $
+      </p>
+      <p>
+        <strong>Description:</strong> {product.description}
+      </p>
+      <p>
+        <strong>Category:</strong> {product.category}
+      </p>
+      <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/Login/LoginPage';
 import ProductsPage from '@/pages/Products/ProductsPage';
 import CartPage from '@/pages/Cart/CartPage';
+import ProductDetailsPage from '@/pages/Products/ProductDetailsPage';
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Navigate to="/login" />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/products" element={<ProductsPage />} />
+    <Route path="/products/:id" element={<ProductDetailsPage />} />
     <Route path="/cart" element={<CartPage />} />
   </Routes>
 );
