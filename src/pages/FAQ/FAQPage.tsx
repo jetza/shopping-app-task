9424298b0904a0f81a0cbd3ee9364e3785e35ebb
Jelena <jelena-miodragovic@live.com>
2025-05-28 17:50:43 +0200

    fix FAQ margin

--- src/pages/FAQ/FAQPage.tsx
+++ src/pages/FAQ/FAQPage.tsx
@@ -1,9 +1,8 @@
 import FAQ from '@/components/FAQ/FAQ';
-import styles from './FAQPage.module.scss';
 
 const FAQPage = () => {
   return (
-    <div className={styles.faqPageContainer}>
+    <div>
       <FAQ />
     </div>
   );
